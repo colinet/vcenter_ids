9d1ccc22feaf1e67884fe6b1af403d033d63feb9
COLINET Remi 00048440 <remi.colinet@3ds.com>
2020-07-02 10:09:04 +0200

    Cleaned commented code

--- vmw.rb
+++ vmw.rb
@@ -9,8 +9,6 @@ require 'credentials'
 require 'resolv.rb'
 require 'net/ping'
 
-require 'iaas_common'
-
 #@debug = true
 @debug = true
 
@@ -24,33 +22,15 @@ def dump_ems()
   ems = $evm.root['ext_management_system']
 
   vcenter_ids = {}
-
-  #$evm.log("info","#{@log_prefix} - EMS:<#{ems.name}> Begin Attributes")
-  #ems.attributes.sort.each { |k, v| $evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> Attributes - #{k}: #{v.inspect}")}
-  #$evm.log("info","#{@log_prefix} - EMS:<#{ems.name}> End Attributes")
-  #$evm.log("info","")
-
-  #$evm.log("info","#{@log_prefix} - EMS:<#{ems.name}> Begin Associations")
-  #ems.associations.sort.each { |assc| $evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> Associations - #{assc}")}
-  #$evm.log("info","#{@log_prefix} - EMS:<#{ems.name}> End Associations")
-  #$evm.log("info","")
-
   vcenter_ids['name'] = ems.name
   $evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> vcenter_ids - #{vcenter_ids.inspect}")
 
 
   $evm.log("info","#{@log_prefix} - EMS:<#{ems.name}> Begin EMS Folders")
-  #vcenter_ids['folder'] = []
   vcenter_ids['folders'] = {}
-  #ems.ems_folders.each { |ef| ef.attributes.sort.each { |k,v| $evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> EMS Folder:<#{ef.name}> #{k}: #{v.inspect}") }}
   ems.ems_folders.each do |ef|
-    #fullpath = ef.folder_path
-    #$evm.log("info","#{@log_prefix} - EMS:<#{fullpath}> Begin EMS Folders")
     ef.attributes.sort.each do |k,v|
       if k == 'id'
-        #vcenter_ids['folder'].append({ "#{ef.name}" => "#{v}" })
-        #vcenter_ids['folder'].append({ "#{ef.folder_path}" => "#{v}" })
-        #vcenter_ids['folder'][ef.folder_path] = "#{v}"
         vcenter_ids['folders']["#{ef.folder_path}"] = "#{v}"
       end
     end
@@ -62,14 +42,10 @@ def dump_ems()
 
 
   $evm.log("info","#{@log_prefix} - EMS:<#{ems.name}> Begin EMS Datacenters")
-  #vcenter_ids['datacenters'] = []
   vcenter_ids['datacenters'] = {}
-  #ems.datacenters.each { |ef| ef.attributes.sort.each { |k,v| $evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> EMS Datacenter:<#{ef.name}> #{k}: #{v.inspect}")}}
   ems.datacenters.each do |ef|
     ef.attributes.sort.each do |k,v|
       if k == 'id'
-        #vcenter_ids['datacenters'].append({ "#{ef.name}" => "#{v}" })
-        #vcenter_ids['datacenters'][ef.name] = "#{v}"
         vcenter_ids['datacenters']["#{ef.name}"] = "#{v}"
       end
     end
@@ -81,14 +57,10 @@ def dump_ems()
 
 
   $evm.log("info","#{@log_prefix} - EMS:<#{ems.name}> Begin EMS Datastores")
-  #vcenter_ids['datastores'] = []
   vcenter_ids['datastores'] = {}
-  #ems.storages.each { |ef| ef.attributes.sort.each { |k,v| $evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> EMS Datastore:<#{ef.name}> #{k}: #{v.inspect}")}}
   ems.storages.each do |ef|
     ef.attributes.sort.each do |k,v|
       if k == 'id'
-        #vcenter_ids['datastores'].append({ ))"#{ef.name}" => "#{v}" })
-        #vcenter_ids['datastores'][ef.name] = "#{v}"
         vcenter_ids['datastores']["#{ef.name}"] = "#{v}"
       end
     end
@@ -100,14 +72,10 @@ def dump_ems()
 
 
   $evm.log("info","#{@log_prefix} - EMS:<#{ems.name}> Begin EMS Hosts")
-  #vcenter_ids['hosts'] = []
   vcenter_ids['hosts'] = {}
-  #ems.hosts.each { |ef| ef.attributes.sort.each { |k,v| $evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> EMS Host:<#{ef.name}> #{k}: #{v.inspect}")}}
   ems.hosts.each do |ef|
     ef.attributes.sort.each do |k,v|
       if k == 'id'
-        #vcenter_ids['hosts'].append({ "#{ef.name}" => "#{v}" })
-        #vcenter_ids['hosts'][ef.name] = "#{v}"
         vcenter_ids['hosts']["#{ef.name}"] = "#{v}"
       end
     end
@@ -119,14 +87,10 @@ def dump_ems()
 
 
   $evm.log("info","#{@log_prefix} - EMS:<#{ems.name}> Begin EMS Clusters")
-  #vcenter_ids['clusters'] = []
   vcenter_ids['clusters'] = {}
-  #ems.ems_clusters.each { |ef| ef.attributes.sort.each { |k,v| $evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> EMS Cluster:<#{ef.name}> #{k}: #{v.inspect}")}}
   ems.ems_clusters.each do |ef|
     ef.attributes.sort.each do |k,v|
       if k == 'id'
-        #vcenter_ids['clusters'].append({ "#{ef.name}" => "#{v}" })
-        #vcenter_ids['clusters'][ef.name] = "#{v}"
         vcenter_ids['clusters']["#{ef.name}"] = "#{v}"
       end
     end
@@ -135,29 +99,6 @@ def dump_ems()
   $evm.log("info","")
 
   $evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> vcenter_ids - #{vcenter_ids.inspect}")
-
-
-  #$evm.log("info","#{@log_prefix} - EMS:<#{ems.name}> Begin Virtual Columns")
-  #ems.virtual_column_names.sort.each { |vcn| $evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> Virtual Columns - #{vcn}: #{ems.send(vcn)}")}
-  #$evm.log("info","#{@log_prefix} - EMS:<#{ems.name}> End Virtual Columns")
-  #$evm.log("info","")
-
-
-  #$evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> EMS: #{ems.inspect}")
-  #ems.ems_folders.each do |ef|
-    #$evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> EMS folder path: #{ef.inspect}")
-    #$evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> EMS child folders ======================================================== ")
-    #$evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> EMS child path: #{ef.folders}")
-    #ef.attributes.sort.each do |k,v|
-    #  if k == 'id'
-    #    vcenter_ids['folder'].append({ "#{ef.name}" => "#{v}" })
-    #  end
-    #end
-    #mypath = ef.folder_path()
-    #$evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> EMS my path: #{mypath}")
-  #end
-  #ems.get_folder_paths { |path| $evm.log("info", "#{@log_prefix} - EMS:<#{ems.name}> EMS path: #{path.inspect}")}
-
 
   return vcenter_ids
 end
@@ -175,8 +116,6 @@ def vmw_show_infos()
 
   vcenter_ids = YAML.load_file(yaml_file)
   $evm.log("info", "#{@log_prefix} - YAML #{vcenter_ids.inspect}")
-  #vcenter_ids = {}
-  #vcenter_ids['vcenter_ids'] = {}
   vcenter_ids['vcenter_ids'][vcenter] = data
   $evm.log("info", "#{@log_prefix} - YAML #{vcenter_ids.inspect}")
 
@@ -187,5 +126,4 @@ def vmw_show_infos()
 
   exit MIQ_OK
 end
-
 
